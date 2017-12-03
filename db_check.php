ebe9a1faffa762273cfd3a361b7892507f40b3ea
JAVISH <devashushukla@gmail.com>
2017-12-03 20:41:37 +0530

    asu

--- db_check.php
+++ db_check.php
@@ -1,8 +1,10 @@
 <?php
+echo "hello chick hello testing";
 	include_once('config.php');
 	$db= new db();
 	$result = $db->getAll('select * from final_billing_table');
 	echo "<pre>";
 	print_r($result);
-	die();
+	die("one");
 ?>
+<!-- shri Radhe -->
